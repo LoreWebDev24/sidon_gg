c10033dbda9590a42c0d6738a807567ee7e88e62
LoreWebDev24 <lorenzo.delre12@gmail.com>
2024-02-27 00:53:04 +0100

    ADD- Function to replace the #

--- src/pages/PlayerDetail.vue
+++ src/pages/PlayerDetail.vue
@@ -5,14 +5,24 @@ import { useRouter } from "vue-router";
 const { slug } = defineProps(["slug"]);
 const playerGames = ref([]);
 
+function replaceHashWithSlash(inputString) {
+
+    const stringWithoutHash = inputString.replace(/#/g, '/');
+
+  return stringWithoutHash;
+}
+
 onMounted(async () => {
   await axios
     .get("http://localhost:4000/past10Games", {
-      params: { username: playerSearch.value },
+      params: { username: replaceHashWithSlash(slug) },
     })
     .then((resp) => {
       playerGames.value = resp.data;
       console.log(playerGames.value);
+      console.log(slug);
+      console.log(replaceHashWithSlash(slug));
+      console.log(typeof(replaceHashWithSlash(slug)))
     })
     .catch((error) => {
       console.log(error);
